27b9b93eb45e779b636d1a8322423bbe67a18266
Kosskak <destin@sjtu.edu.cn>
2019-12-17 01:05:25 +0800

    class KeyWord

---
+++ word.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 15 19:30:20 2019
+
+@author: zyb32
+"""
+
+class KeyWord():
+    def __init__(self,name,tp):
+        self.name = name
+        self.type = tp
